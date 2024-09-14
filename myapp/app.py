ddc2c188e1ea1f603ff8a9e40b700073e2985a88
lmkawakami <lmkawakami@hotmail.com>
2024-09-14 01:14:54 -0300

    chore: Add input slider for 'x' in app.py to calculate x²

--- myapp/app.py
+++ myapp/app.py
@@ -3,8 +3,13 @@ from shiny.express import input
 
 ui.panel_title("Hello Shiny!")
 ui.input_slider("n", "N", 0, 100, 20)
+ui.input_slider("x", "X", 0, 100, 20)
 
 
 @render.text
-def txt():
+def txt_n():
     return f"n*2 is {input.n() * 2}"
+
+@render.text
+def txt_x():
+    return f"x² is {input.x() ** 2}"
